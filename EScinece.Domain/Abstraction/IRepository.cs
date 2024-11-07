6c5c3d6d525ef73736947e59d9a0a44eb50d0bb6
Constantine <svedinkvidon@gmail.com>
2024-11-07 19:45:29 +0500

    Добавил репозиторий для статей

---
+++ EScinece.Domain/Abstraction/IRepository.cs
@@ -0,0 +1,10 @@
+namespace EScinece.Domain.Abstraction;
+
+public interface IRepository<T> where T : BaseEntity
+{
+    public Task<T?> GetById(Guid id);
+    public Task<List<T>> GetAll();
+    public Task<T> Create(T entity);
+    public Task<T> Update(T entity);
+    public Task<Guid> Delete(T entity);
+}
