70536b60d4bd46b7a4707b4992e0e2252efd440f
Constantine <svedinkvidon@gmail.com>
2024-11-07 18:50:01 +0500

    database migration

--- EScience.Application/Program.cs
+++ EScience.Application/Program.cs
@@ -1,8 +1,17 @@
+using EScinece.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<EScienceDbContext>(options =>
+{
+    options.UseNpgsql(configuration.GetConnectionString(nameof(EScienceDbContext)));
+});
 
 var app = builder.Build();
 
